52fb3c5cfe500cdb8c1ac842548e4ea9acb14a4f
Poylar <doaleks8@gmail.com>
2023-08-18 13:57:27 +0800

    fix style project

--- src/assets/js/Animations.js
+++ src/assets/js/Animations.js
@@ -1,269 +1,269 @@
-import { gsap } from 'gsap';
-import CustomEase from 'gsap/CustomEase';
-import SplitText from 'gsap/SplitText';
-import ScrollTrigger from 'gsap/ScrollTrigger';
-import breakpoints from './MatchMedia';
+import { gsap } from 'gsap'
+import CustomEase from 'gsap/CustomEase'
+import SplitText from 'gsap/SplitText'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+import breakpoints from './MatchMedia'
 
-gsap.registerPlugin(SplitText, ScrollTrigger, CustomEase, matchMedia);
+gsap.registerPlugin(SplitText, ScrollTrigger, CustomEase, matchMedia)
 
 const globalGap = parseInt(
-  getComputedStyle(document.body).getPropertyValue('--gap'),
-  10
-);
+	getComputedStyle(document.body).getPropertyValue('--gap'),
+	10
+)
 
-const mm = gsap.matchMedia();
+const mm = gsap.matchMedia()
 
 function aboutChonologySliderAnimation() {
-  const aboutSliderItem = document.querySelector('.about-slider__slide');
-  if (!aboutSliderItem) return;
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: '.about-slider-wrapper',
-      start: 'top center',
-      end: 'center center',
-    },
-  });
-  tl.to('.about-slider-line__active-line', {
-    ease: CustomEase.create('cubic', '0.96, 0.02, 0.34, 0.99'),
-    width: aboutSliderItem.offsetWidth - globalGap,
-  });
+	const aboutSliderItem = document.querySelector('.about-slider__slide')
+	if (!aboutSliderItem) return
+	const tl = gsap.timeline({
+		scrollTrigger: {
+			trigger: '.about-slider-wrapper',
+			start: 'top center',
+			end: 'center center',
+		},
+	})
+	tl.to('.about-slider-line__active-line', {
+		ease: CustomEase.create('cubic', '0.96, 0.02, 0.34, 0.99'),
+		width: aboutSliderItem.offsetWidth - globalGap,
+	})
 }
 function animationAfterLoaded() {
-  mm.add(breakpoints.isDesktop, () => {
-    const textSplit = new SplitText('.hero__title', {
-      type: 'chars, lines',
-      charsClass: 'oh',
-      linesClass: 'oh',
-    });
+	mm.add(breakpoints.isDesktop, () => {
+		const textSplit = new SplitText('.hero__title', {
+			type: 'chars, lines',
+			charsClass: 'oh',
+			linesClass: 'oh',
+		})
 
-    const { chars } = textSplit;
-    const h1Split = new SplitText('h1', {
-      type: 'chars, lines',
-      charsClass: 'oh',
-      linesClass: 'oh',
-    });
-    const heroContent = document.querySelector('.hero__content');
-    const tl = gsap.timeline();
+		const { chars } = textSplit
+		const h1Split = new SplitText('h1', {
+			type: 'chars, lines',
+			charsClass: 'oh',
+			linesClass: 'oh',
+		})
+		const heroContent = document.querySelector('.hero__content')
+		const tl = gsap.timeline()
 
-    tl.fromTo(
-      heroContent,
-      {
-        backgroundSize: '140%',
-        backgroundPosition: 'top center',
-      },
-      {
-        duration: 1,
-        backgroundSize: '120%',
-      },
-      '='
-    );
-    tl.fromTo(
-      chars,
-      {
-        y: 100,
-      },
-      {
-        stagger: 0.01,
-        y: 0,
-      }
-    );
-    tl.fromTo(
-      h1Split.chars,
-      {
-        y: 100,
-      },
-      {
-        stagger: 0.01,
-        y: 0,
-      },
-      '<'
-    );
-  });
+		tl.fromTo(
+			heroContent,
+			{
+				backgroundSize: '140%',
+				backgroundPosition: 'top center',
+			},
+			{
+				duration: 1,
+				backgroundSize: '120%',
+			},
+			'='
+		)
+		tl.fromTo(
+			chars,
+			{
+				y: 100,
+			},
+			{
+				stagger: 0.01,
+				y: 0,
+			}
+		)
+		tl.fromTo(
+			h1Split.chars,
+			{
+				y: 100,
+			},
+			{
+				stagger: 0.01,
+				y: 0,
+			},
+			'<'
+		)
+	})
 }
 
 function textAnimation() {
-  const targetArr = [
-    'p',
-    '.news-item__title',
-    '.services-list__title-desc',
-    '.editor-content h1',
-    '.editor-content h2',
-    '.editor-content h3',
-    '.editor-content h4',
-    '.editor-content h5',
-    '.editor-content h6',
-    '.editor-content  li',
-  ];
+	const targetArr = [
+		'p',
+		'.news-item__title',
+		'.services-list__title-desc',
+		'.editor-content h1',
+		'.editor-content h2',
+		'.editor-content h3',
+		'.editor-content h4',
+		'.editor-content h5',
+		'.editor-content h6',
+		'.editor-content  li',
+	]
 
-  const titles = document.querySelectorAll(
-    '.section-heading__title, .services-list__title'
-  );
-  const plainText = document.querySelectorAll(String(targetArr));
-  titles.forEach((title) => {
-    const headingSplit = new SplitText(title, {
-      type: 'chars, lines',
-      charsClass: 'oh',
-      linesClass: 'oh',
-    });
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: title,
-        start: 'top 85%',
-      },
-    });
-    tl.fromTo(
-      headingSplit.chars,
-      {
-        y: 200,
-      },
-      {
-        duration: 0.8,
-        y: 0,
+	const titles = document.querySelectorAll(
+		'.section-heading__title, .services-list__title'
+	)
+	const plainText = document.querySelectorAll(String(targetArr))
+	titles.forEach(title => {
+		const headingSplit = new SplitText(title, {
+			type: 'chars, lines',
+			charsClass: 'oh',
+			linesClass: 'oh',
+		})
+		const tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: title,
+				start: 'top 85%',
+			},
+		})
+		tl.fromTo(
+			headingSplit.chars,
+			{
+				y: 200,
+			},
+			{
+				duration: 0.8,
+				y: 0,
 
-        stagger: 0.01,
-      }
-    );
-  });
-  plainText.forEach((text) => {
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: text,
-        start: 'top 85%',
-      },
-    });
-    tl.fromTo(
-      text,
-      {
-        y: 10,
-        opacity: 0,
-      },
-      {
-        y: 0,
-        opacity: 1,
-      }
-    );
-  });
+				stagger: 0.01,
+			}
+		)
+	})
+	plainText.forEach(text => {
+		const tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: text,
+				start: 'top 85%',
+			},
+		})
+		tl.fromTo(
+			text,
+			{
+				y: 10,
+				opacity: 0,
+			},
+			{
+				y: 0,
+				opacity: 1,
+			}
+		)
+	})
 }
 
 function onLoadAnimation() {
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: '.hero__content',
-      start: 'top top',
-      end: '100%',
-      scrub: 2,
-    },
-  });
-  tl.to('.hero__content', {
-    backgroundPositionY: '100%',
-  });
+	const tl = gsap.timeline({
+		scrollTrigger: {
+			trigger: '.hero__content',
+			start: 'top top',
+			end: '100%',
+			scrub: 2,
+		},
+	})
+	tl.to('.hero__content', {
+		backgroundPositionY: '100%',
+	})
 }
 function Preloader() {
-  const preloader = document.querySelector('.preloader');
-  const preloaderLogo = document.querySelector('.preloader .logo__src');
+	const preloader = document.querySelector('.preloader')
+	const preloaderLogo = document.querySelector('.preloader .logo__src')
 
-  const tl = gsap.timeline();
-  if (!preloader) {
-    animationAfterLoaded();
-    return;
-  }
-  setTimeout(() => {
-    tl.to(preloader, {
-      yPercent: -100,
-      duration: 1,
-      ease: CustomEase.create('cubic', '0.96, 0.02, 0.34, 0.99'),
-    });
-    tl.to(
-      preloaderLogo,
-      {
-        duration: 1,
-        yPercent: -100,
-      },
-      '<'
-    );
+	const tl = gsap.timeline()
+	if (!preloader) {
+		animationAfterLoaded()
+		return
+	}
+	setTimeout(() => {
+		tl.to(preloader, {
+			yPercent: -100,
+			duration: 1,
+			ease: CustomEase.create('cubic', '0.96, 0.02, 0.34, 0.99'),
+		})
+		tl.to(
+			preloaderLogo,
+			{
+				duration: 1,
+				yPercent: -100,
+			},
+			'<'
+		)
 
-    tl.then(animationAfterLoaded());
-  }, 500);
+		tl.then(animationAfterLoaded())
+	}, 500)
 }
 
 function servicesGridAnimation() {
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: '.services-grid',
-      start: 'top center',
-    },
-  });
+	const tl = gsap.timeline({
+		scrollTrigger: {
+			trigger: '.services-grid',
+			start: 'top center',
+		},
+	})
 
-  tl.fromTo(
-    '.services-grid__item',
-    {
-      opacity: 0,
-      scale: 0,
-    },
-    {
-      stagger: {
-        each: 0.1,
-        from: [0, 0],
-        grid: 'auto',
-      },
-      opacity: 1,
-      scale: 1,
-    }
-  );
+	tl.fromTo(
+		'.services-grid__item',
+		{
+			opacity: 0,
+			scale: 0,
+		},
+		{
+			stagger: {
+				each: 0.1,
+				from: [0, 0],
+				grid: 'auto',
+			},
+			opacity: 1,
+			scale: 1,
+		}
+	)
 }
 
 function partnersAnimation() {
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: '.partners-list',
-      start: 'top center',
-    },
-  });
+	const tl = gsap.timeline({
+		scrollTrigger: {
+			trigger: '.partners-list',
+			start: 'top center',
+		},
+	})
 
-  tl.fromTo(
-    '.partners-list__item',
-    {
-      opacity: 0,
-      scale: 0,
-    },
-    {
-      stagger: {
-        each: 0.1,
-        from: [0, 0],
-        grid: 'auto',
-      },
-      opacity: 1,
-      scale: 1,
-    }
-  );
+	tl.fromTo(
+		'.partners-list__item',
+		{
+			opacity: 0,
+			scale: 0,
+		},
+		{
+			stagger: {
+				each: 0.1,
+				from: [0, 0],
+				grid: 'auto',
+			},
+			opacity: 1,
+			scale: 1,
+		}
+	)
 }
 
 const sidebarHeight = document
-  .querySelector('.project-content__sidebar')
-  ?.getBoundingClientRect().height;
+	.querySelector('.project-content__sidebar')
+	?.getBoundingClientRect().height
 function stickyBlocks() {
-  const contentHeight = document.querySelector(
-    '.project-content__gallery'
-  )?.offsetHeight;
-  gsap.timeline({
-    scrollTrigger: {
-      trigger: '.project-content__sidebar ',
-      start: '5% 5%',
-      pin: true,
-      end: `+=${contentHeight - sidebarHeight}`,
-      scrub: true,
-    },
-  });
+	const contentHeight = document.querySelector(
+		'.project-content__gallery'
+	)?.offsetHeight
+	gsap.timeline({
+		scrollTrigger: {
+			trigger: '.project-content__sidebar ',
+			start: '5% 5%',
+			pin: true,
+			end: `+=${contentHeight - sidebarHeight}`,
+			scrub: true,
+		},
+	})
 }
 
 window.addEventListener('load', () => {
-  Preloader();
-  onLoadAnimation();
-  aboutChonologySliderAnimation();
-  textAnimation();
-  servicesGridAnimation();
-  partnersAnimation();
-  if (window.matchMedia(breakpoints.isDesktop).matches) {
-    stickyBlocks();
-  }
-});
+	Preloader()
+	onLoadAnimation()
+	aboutChonologySliderAnimation()
+	textAnimation()
+	servicesGridAnimation()
+	partnersAnimation()
+	if (window.matchMedia(breakpoints.isDesktop).matches) {
+		stickyBlocks()
+	}
+})
--- src/assets/js/Sliders.js
+++ src/assets/js/Sliders.js
@@ -1,150 +1,149 @@
-/* eslint-disable import/no-unresolved */
-import Swiper, { Scrollbar, Thumbs, EffectFade } from 'swiper';
-import 'swiper/scss';
-import 'swiper/scss/thumbs';
-import 'swiper/scss/effect-fade';
-import breakpoints from './MatchMedia';
+import Swiper, { Scrollbar, Thumbs, EffectFade } from 'swiper'
+import 'swiper/scss'
+import 'swiper/scss/thumbs'
+import 'swiper/scss/effect-fade'
+import breakpoints from './MatchMedia'
 
 function sliders() {
-  const portfolioSlider = document.querySelector('.portfolio-slider');
-  if (portfolioSlider) {
-    const portfolioInstance = new Swiper(portfolioSlider, {
-      modules: [Scrollbar],
-      slidesPerView: 1,
-      spaceBetween: 30,
-      scrollbar: {
-        el: portfolioSlider.querySelector('.slider-scrollbar'),
-        draggable: true,
-      },
-      breakpoints: {
-        768: {
-          slidesPerView: 2,
-        },
-      },
-    });
-    portfolioInstance.init();
-  }
-  const projectsSlider = document.querySelector('.other-projects');
-  if (projectsSlider) {
-    const projectsInstance = new Swiper(projectsSlider, {
-      modules: [Scrollbar],
-      slidesPerView: 1,
-      spaceBetween: 30,
-      scrollbar: {
-        el: projectsSlider.querySelector('.slider-scrollbar'),
-        draggable: true,
-      },
-      breakpoints: {
-        768: {
-          slidesPerView: 2,
-        },
-        1440: {
-          slidesPerView: 3,
-        },
-      },
-    });
-    projectsInstance.init();
-  }
+	const portfolioSlider = document.querySelector('.portfolio-slider')
+	if (portfolioSlider) {
+		const portfolioInstance = new Swiper(portfolioSlider, {
+			modules: [Scrollbar],
+			slidesPerView: 1,
+			spaceBetween: 30,
+			scrollbar: {
+				el: portfolioSlider.querySelector('.slider-scrollbar'),
+				draggable: true,
+			},
+			breakpoints: {
+				768: {
+					slidesPerView: 2,
+				},
+			},
+		})
+		portfolioInstance.init()
+	}
+	const projectsSlider = document.querySelector('.other-projects')
+	if (projectsSlider) {
+		const projectsInstance = new Swiper(projectsSlider, {
+			modules: [Scrollbar],
+			slidesPerView: 1,
+			spaceBetween: 30,
+			scrollbar: {
+				el: projectsSlider.querySelector('.slider-scrollbar'),
+				draggable: true,
+			},
+			breakpoints: {
+				768: {
+					slidesPerView: 2,
+				},
+				1440: {
+					slidesPerView: 3,
+				},
+			},
+		})
+		projectsInstance.init()
+	}
 
-  const careerSlider = document.querySelector('.js-career-slider');
-  if (careerSlider) {
-    const careerSliderInstance = new Swiper(careerSlider, {
-      modules: [Scrollbar],
-      slidesPerView: 1,
-      init: false,
-      scrollbar: {
-        el: careerSlider.querySelector('.slider-scrollbar'),
-        draggable: true,
-      },
-    });
-    if (window.matchMedia(breakpoints.isMobile).matches) {
-      careerSliderInstance.init();
-    }
-  }
+	const careerSlider = document.querySelector('.js-career-slider')
+	if (careerSlider) {
+		const careerSliderInstance = new Swiper(careerSlider, {
+			modules: [Scrollbar],
+			slidesPerView: 1,
+			init: false,
+			scrollbar: {
+				el: careerSlider.querySelector('.slider-scrollbar'),
+				draggable: true,
+			},
+		})
+		if (window.matchMedia(breakpoints.isMobile).matches) {
+			careerSliderInstance.init()
+		}
+	}
 
-  const aboutSlider = document.querySelector('.js-about-slider');
-  if (aboutSlider) {
-    const aboutSliderInstanceText = new Swiper('.js-about-slider-text', {
-      modules: [EffectFade],
-      slidesPerView: 1,
-      effect: 'fade',
-      fadeEffect: {
-        crossFade: true,
-      },
-    });
-    const aboutSliderInstance = new Swiper(aboutSlider, {
-      modules: [Thumbs, Scrollbar],
-      slidesPerView: 1.2,
-      scrollbar: {
-        el: aboutSlider.querySelector('.slider-scrollbar'),
-        draggable: true,
-      },
-      thumbs: {
-        swiper: aboutSliderInstanceText,
-      },
-      breakpoints: {
-        768: {
-          slidesPerView: 2.2,
-        },
-      },
-    });
-    aboutSliderInstance.init();
-  }
+	const aboutSlider = document.querySelector('.js-about-slider')
+	if (aboutSlider) {
+		const aboutSliderInstanceText = new Swiper('.js-about-slider-text', {
+			modules: [EffectFade],
+			slidesPerView: 1,
+			effect: 'fade',
+			fadeEffect: {
+				crossFade: true,
+			},
+		})
+		const aboutSliderInstance = new Swiper(aboutSlider, {
+			modules: [Thumbs, Scrollbar],
+			slidesPerView: 1.2,
+			scrollbar: {
+				el: aboutSlider.querySelector('.slider-scrollbar'),
+				draggable: true,
+			},
+			thumbs: {
+				swiper: aboutSliderInstanceText,
+			},
+			breakpoints: {
+				768: {
+					slidesPerView: 2.2,
+				},
+			},
+		})
+		aboutSliderInstance.init()
+	}
 
-  const aboutGallery = document.querySelector('.js-about-gallery');
-  if (aboutGallery) {
-    const aboutGalleryInstance = new Swiper(aboutGallery, {
-      modules: [Scrollbar],
-      slidesPerView: 1,
-      init: false,
-      scrollbar: {
-        el: aboutGallery.querySelector('.slider-scrollbar'),
-        draggable: true,
-      },
-    });
-    if (window.matchMedia(breakpoints.isMobile).matches) {
-      aboutGalleryInstance.init();
-    }
-  }
+	const aboutGallery = document.querySelector('.js-about-gallery')
+	if (aboutGallery) {
+		const aboutGalleryInstance = new Swiper(aboutGallery, {
+			modules: [Scrollbar],
+			slidesPerView: 1,
+			init: false,
+			scrollbar: {
+				el: aboutGallery.querySelector('.slider-scrollbar'),
+				draggable: true,
+			},
+		})
+		if (window.matchMedia(breakpoints.isMobile).matches) {
+			aboutGalleryInstance.init()
+		}
+	}
 
-  const reviewsSlider = document.querySelector('.js-reviews-slider');
-  if (reviewsSlider) {
-    const reviewSliderInstance = new Swiper(reviewsSlider, {
-      modules: [Scrollbar],
-      slidesPerView: 1,
-      scrollbar: {
-        el: reviewsSlider.querySelector('.slider-scrollbar'),
-        draggable: true,
-      },
-      breakpoints: {
-        768: {
-          slidesPerView: 3,
-          spaceBetween: 90,
-        },
-      },
-    });
-    reviewSliderInstance.init();
-  }
-  const newsSlider = document.querySelector('.js-news-slider');
-  if (newsSlider) {
-    const newsSliderInstance = new Swiper(newsSlider, {
-      modules: [Scrollbar],
-      slidesPerView: 1,
-      scrollbar: {
-        el: newsSlider.querySelector('.slider-scrollbar'),
-        draggable: true,
-      },
-      breakpoints: {
-        768: {
-          slidesPerView: 3,
-          spaceBetween: 30,
-        },
-      },
-    });
-    newsSliderInstance.init();
-  }
+	const reviewsSlider = document.querySelector('.js-reviews-slider')
+	if (reviewsSlider) {
+		const reviewSliderInstance = new Swiper(reviewsSlider, {
+			modules: [Scrollbar],
+			slidesPerView: 1,
+			scrollbar: {
+				el: reviewsSlider.querySelector('.slider-scrollbar'),
+				draggable: true,
+			},
+			breakpoints: {
+				768: {
+					slidesPerView: 3,
+					spaceBetween: 90,
+				},
+			},
+		})
+		reviewSliderInstance.init()
+	}
+	const newsSlider = document.querySelector('.js-news-slider')
+	if (newsSlider) {
+		const newsSliderInstance = new Swiper(newsSlider, {
+			modules: [Scrollbar],
+			slidesPerView: 1,
+			scrollbar: {
+				el: newsSlider.querySelector('.slider-scrollbar'),
+				draggable: true,
+			},
+			breakpoints: {
+				768: {
+					slidesPerView: 3,
+					spaceBetween: 30,
+				},
+			},
+		})
+		newsSliderInstance.init()
+	}
 }
 
-sliders();
+sliders()
 
-window.addEventListener('resize', sliders);
+window.addEventListener('resize', sliders)
