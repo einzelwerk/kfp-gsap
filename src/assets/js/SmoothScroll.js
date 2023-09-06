28292c9f198f55c5fa12d65930683cefb8cf28ac
Poylar <doaleks8@gmail.com>
2023-09-06 20:07:31 +0800

    deploy

--- src/assets/js/SmoothScroll.js
+++ src/assets/js/SmoothScroll.js
@@ -1,18 +1,17 @@
-import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
-import ScrollSmoother from 'gsap/ScrollSmoother';
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+import ScrollSmoother from 'gsap/ScrollSmoother'
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 const smoother = ScrollSmoother.create({
-  smooth: 1,
-  effects: true,
-  smoothTouch: 0.1,
-});
+	smooth: 1,
+	effects: true,
+})
 
-document.querySelectorAll('a[href^="#"]').forEach((link) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    const tg = e.target.closest('a').getAttribute('href');
-    smoother.scrollTo(tg, true, 'top top');
-  });
-});
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+	link.addEventListener('click', e => {
+		e.preventDefault()
+		const tg = e.target.closest('a').getAttribute('href')
+		smoother.scrollTo(tg, true, 'top top')
+	})
+})
